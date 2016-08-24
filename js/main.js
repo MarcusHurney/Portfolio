90b0d66e6cf8c3ce29220bbde5b8e2755c836e1a
Marcus Hurney <marcushurney@gmail.com>
2016-08-24 01:31:56 -0400

    Completes all sections. Adds animation to about section. Adds smooth scroll to entire website. Adds media queries for the cover section.

---
+++ js/main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+
+  // implement sticky navbar
+  $(window).scroll(function() {
+    // if the user scrolls past the "cover" section
+    // make the navbar stick to the top of the screen
+    if($(window).scrollTop() > $(window).height()) {
+      $('.navbar').addClass('navbar-fixed-top');
+    }
+    // remove the navbar if the user scrolls back onto
+    // the "cover" section
+    if($(window).scrollTop() < $(window).height()) {
+      $('.navbar').removeClass('navbar-fixed-top');
+    }
+
+  });
+
+  // implement smooth scroll for when a user clicks a nav button
+  $('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 700, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+
+  // add fadeInDown animation
+  $('.js-wp-1').waypoint(function(direction) {
+    $('.js-wp-1').addClass('animated fadeInDown');
+  }, {
+    // offset determines when the animation should start
+    offset: '50%'
+  });
+
+  // add fadeInLeft animation
+  $('.js-wp-2').waypoint(function(direction) {
+    $('.js-wp-2').addClass('animated fadeInLeft');
+  }, {
+    // offset determines when the animation should start
+    offset: '50%'
+  });
+
+  // add fadeInRight animation
+  $('.js-wp-3').waypoint(function(direction) {
+    $('.js-wp-3').addClass('animated fadeInRight');
+  }, {
+    // offset determines when the animation should start
+    offset: '50%'
+  });
+
+  // add fadeInUp animation
+  $('.js-wp-4').waypoint(function(direction) {
+    $('.js-wp-4').addClass('animated fadeInUp');
+  }, {
+    // offset determines when the animation should start
+    offset: '50%'
+  });
+
+
+
+
+
+});
